0ffe653a5eb989ff81ceb7f327c7389c7fac7d1a
abdealijaroli <jaroliabdeali@gmail.com>
2021-06-16 23:57:26 +0530

    Button events initial

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types' 
+
+const Button = ({ text, color, onClick }) => {
+    
+    return (
+    <button onClick= {onClick} style={{ backgroundColor: color}} className='btn'>
+        {text}
+    </button>
+    )
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onclick: PropTypes.func
+}
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import Button from './Button'
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log("clicked");
+    }
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <button className='btn'>Add</button> 
+            <Button color='#27D507' text= 'Add' onClick = {onClick} />
         </header>
     )
 }
