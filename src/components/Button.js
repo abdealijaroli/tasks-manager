e797c1496c088a575933687a732dd10be8a1a18f
abdealijaroli <abdealiburu03@gmail.com>
2021-06-17 00:03:53 +0530

    Minor changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types' 
 
 const Button = ({ text, color, onClick }) => {
-    
     return (
     <button onClick= {onClick} style={{ backgroundColor: color}} className='btn'>
         {text}
