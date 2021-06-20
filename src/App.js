099aed32445825d4c78e1f1c927b5db056afd8fc
abdealijaroli <abdealiburu03@gmail.com>
2021-06-20 20:56:54 +0530

    Delete task

--- src/App.js
+++ src/App.js
@@ -23,10 +23,17 @@ function App() {
         reminder: false,
     }
 ])
+
+  //Delete task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container"> 
         <Header />
-        <Tasks tasks={tasks}/>
+        <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,14 @@
 import { FaTimesCircle } from 'react-icons/fa'
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
     return (
         <div className='task'>
             <h3>
-                {task.text} 
-                <FaTimesCircle style={{color: 'red', float: 'right', cursor: 'pointer'}}
+                {task.text}
+                <FaTimesCircle 
+                style={{color: 'red', 
+                float: 'right', 
+                cursor: 'pointer'}} onClick={()=>onDelete(task.id)}
                 />
             </h3>
             <p>{task.day}</p>
