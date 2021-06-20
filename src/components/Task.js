2ac0addd678ca26fad775720f259e192cea417c7
abdealijaroli <abdealiburu03@gmail.com>
2021-06-20 13:12:02 +0530

    react icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,13 @@
+import { FaTimesCircle } from 'react-icons/fa'
+
 const Task = ({ task }) => {
     return (
         <div className='task'>
-            <h3>{task.text}</h3>
+            <h3>
+                {task.text} 
+                <FaTimesCircle style={{color: 'red', float: 'right', cursor: 'pointer'}}
+                />
+            </h3>
             <p>{task.day}</p>
         </div>
     )
