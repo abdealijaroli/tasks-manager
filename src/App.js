12fc326d1a9e578c82bdfa49157d42a6dde54782
abdealijaroli <abdealiburu03@gmail.com>
2021-06-21 00:05:15 +0530

    Added reminder task feature

--- src/App.js
+++ src/App.js
@@ -30,10 +30,20 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task)=> task.id === id ? 
+    ({...task, reminder: !task.reminder}) : 
+    task))
+  }
+
   return (
     <div className="container"> 
         <Header />
-        <Tasks tasks={tasks} onDelete={deleteTask}/>
+        {tasks.length > 0 ? (
+          <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+          ) : (
+          "No tasks to show.") }
     </div>
   );
 }
